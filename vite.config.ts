51448a4efb755ace6c454d424df8436fed2c6ac4
codexcancerion <melbertmarafo2022@gmail.com>
2026-06-05 10:42:25 +0800

    break

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
@@ -9,7 +8,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react()],
     resolve: {
       alias: {
         "@": resolve(__dirname, "./src"),
